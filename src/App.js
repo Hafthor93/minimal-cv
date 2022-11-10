466ebbd722635b31e8b76bc457c8e9d26c5f73f3
Hafthor93 <101139553+Hafthor93@users.noreply.github.com>
2022-11-10 16:56:12 +0000

    asdf

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const HeaderText = (props) => {
+  return (
+    <div>
+      <p className='header-name'>{props.name}</p>
+      <p className='header-text'>{props.text}</p>
+    </div>
+  )
+}
+
+
+
+const myObject = {
+  name: "Hafþór Frímannsson",
+  text: "Rookie programmer",
+  jobs: [
+    {
+      title: "atNorth",
+      date: "October 2019 - Present",
+      description : "Technician. Repairing Bitcoin/Ethereum machines."
+    },
+    {
+      title: "Airport Associates",
+      date: "April 2018 - September 2019 ",
+      description : "Load Control. calculating aircraft weight, balance and control of wide body aircraft to determine load limits."
+    },
+    {
+      title: "Airport Associates",
+      date: "May 2015 - April 2018",
+      description : "Baggage Handler. Loading and unloading baggages."
+    },
+  ]
+}
+
+
+const jobList = (jobs) => {
+  return (
+    <div className='jobList'>
+      <h1>{jobs.title}</h1>
+      <h2>{jobs.date}</h2>
+      <p>{jobs.description}</p>
+    </div>
+  )
+}
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        <img className="image" src="me.png"></img>
+        <h1><HeaderText name={myObject.name}/></h1>
+        <p className="HeaderP"><HeaderText text={myObject.text}/></p>
+        </div>
+        <hr></hr>
+        <div>
+        <h1>{myObject.jobs[0].title}</h1>
+        <h2>{}</h2>
+        <p>{}</p>
+        </div>
       </header>
     </div>
   );
